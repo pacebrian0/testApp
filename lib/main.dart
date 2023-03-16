b5cccf789a1b22aa7b7832221931ac4ca3766a0c
Brian Pace <pacebrian0@gmail.com>
2023-03-16 23:29:31 +0100

    init

--- lib/main.dart
+++ lib/main.dart
@@ -2,28 +2,39 @@ import 'package:flutter/material.dart';
 
 
 void main() => runApp(MaterialApp(
-  home: Scaffold(
+  home: Home()
+));
+
+class Home extends StatelessWidget {
+  const Home({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
     appBar: AppBar(
-      title: Text("Hello Rebecca"),
+      title: Text("Hello Rebecca!!!!!"),
       centerTitle: true,
       backgroundColor: Colors.blue.shade500,
     ),
     body: Center(
-      child: Text(
-        "Hello Rebecca!",
-        style: TextStyle(
-          fontSize: 26,
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2,
-          color: Colors.grey.shade400,
-          fontFamily: 'IndieFlower'
-        ),
-      ),
+      // child: Text(
+      //   "Hello Rebecca!",
+      //   style: TextStyle(
+      //     fontSize: 26,
+      //     fontWeight: FontWeight.bold,
+      //     letterSpacing: 2,
+      //     color: Colors.grey.shade400,
+      //     fontFamily: 'IndieFlower'
+      //   ),
+      child: Image.asset("assets/Cat2.jpg"),
+
+
     ),
     floatingActionButton: FloatingActionButton(
       child: Text("+"),
       onPressed: () => {},
       backgroundColor: Colors.red.shade900,
     ),
-  ),
-));
+  );
+  }
+}
